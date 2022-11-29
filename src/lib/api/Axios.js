f4230e099a9c0a74cffa6aaca5e0a7453a386086
Marvin Toro <ingricardotoro@gmail.com>
2022-11-29 10:56:21 -0600

    add cors

--- src/lib/api/Axios.js
+++ src/lib/api/Axios.js
@@ -9,12 +9,12 @@ export default axios.create({
     baseURL: BASE_URL, //'https://poswebbackend-dataplus.up.railway.app/api/',
     headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': true
+        'Access-Control-Allow-Origin': '*'
     }
 });
 
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL, //'https://poswebbackend-dataplus.up.railway.app/api/',
     headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
+    withCredentials: '*'
 });
