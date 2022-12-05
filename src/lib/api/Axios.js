118ef17afb6f58a87b9888954f653b3f59069b74
Marvin Toro <ingricardotoro@gmail.com>
2022-12-04 18:13:57 -0600

    base URL

--- src/lib/api/Axios.js
+++ src/lib/api/Axios.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 //const { VITE_API_URL } = getEnvVariables();
 
-const BASE_URL = 'https://poswebbackend-production.up.railway.app/api';
+const BASE_URL = 'https://api.dataplushn.com/api';
+//const BASE_URL = 'https://poswebbackend-production.up.railway.app/api';
 //const BASE_URL = 'http://localhost:1338/api';
 
 export default axios.create({
